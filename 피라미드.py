3c35f201a77eca9f9b0591fa740deeb66d182a1e
Ydaehan <108657384+Ydaehan@users.noreply.github.com>
2022-07-04 16:59:18 +0900

    Add files via upload

---
+++ 피라미드.py
@@ -0,0 +1,13 @@
+# 속이 빈 사각형
+
+"""
+floor = int(input("floor = "))
+
+for row in range(int(floor/2)):
+    for col in range(floor):
+        if col == 0 or col == floor -1 or row == int(floor/2)-1 or row == 0:
+            print("*",end="")
+        else:
+            print(" ",end="")
+    print()
+"""
