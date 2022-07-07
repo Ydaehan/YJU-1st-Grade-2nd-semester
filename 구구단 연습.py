b4f442fc00e678d79c02d5bc10eb5740d45286f7
Ydaehan <108657384+Ydaehan@users.noreply.github.com>
2022-07-07 10:19:38 +0900

    Add files via upload

--- 구구단 연습.py
+++ 구구단 연습.py
@@ -9,20 +9,18 @@ while True:
     inputValue = int(input())
     if inputValue == 1:
         print("출력할 구구단의 단을 입력하세요. 구구단의 단은 2~9 사이 입력")
-        while True: 
+        while True:
             inputNum = int(input())
-            if inputNum <= 1 or inputNum >= 10 :
-                print("2~9사이 정수를 입력해주세요.")
+            if inputNum <= 1 or inputNum >= 10:
+                print("2~9사이 정수를 입력해주세요")
                 continue
             else:
-                for num in range(1,10):
-                    print(inputNum,"X",num,"=",inputNum*num)
+                for count in range(1,10):
+                    print(inputNum,"X",count,"=",inputNum*count)
                 break
     elif inputValue == 2:
         print("이용해주셔서 감사합니다.")
         break
     else:
-        print("잘못 입력하셨습니다. 다시입력하세요.")
+        print("잘못 입력하셨습니다. 다시 입력하세요.")
         continue
-
-
